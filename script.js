0a6363c592f4fa559ea555c8002e4353e02adc1e
AYAN CHOWDHURY <145857268+thegreatayanchowdhury@users.noreply.github.com>
2024-09-29 12:10:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleNav() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+}
+
+// Typing effect
+let i = 0;
+const text = "I am a AI & ML Specialist and AIML Core Member at GDG on Campus AEC";
+const speed = 100;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("element").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+window.onload = function() {
+    const element = document.getElementById("element");
+    if (element) {
+        typeEffect();
+    }
+};
+
